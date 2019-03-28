5c366849fe7c950ba6b7831f58761f8dfc38d391
D7703_24 <d7703_24@d7703-24ui-imac.local>
2019-03-28 15:25:50 +0900

    12345 12345

--- Alien imageView02/ViewController.swift
+++ Alien imageView02/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienImage: UIImageView!
+    @IBOutlet weak var CountLabel: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImage.image = UIImage(named: "frame1.png")
+        CountLabel.text = "frame\(count).png"
     }
+    
+    @IBAction func ButtonPressed(_ sender: Any) {
+        count+=1
+        alienImage.image = UIImage(named:"frame\(count).png")
+        CountLabel.text = "frame\(count).png"
+        if(count == 5){
+        count = 0
+    }
+    
 
-
+    }
 }
-
